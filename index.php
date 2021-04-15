fbfcc5416f2cfa62af1d46d4291a51e4cf9150eb
bin1688 <bchen3@my.waketech.edu>
2021-04-14 20:25:08 -0400

    first

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+	$operatingSys = $_POST['os'];
+	$numberOfCopy = $_POST['numCopies'];
+	$subtotal = $numberOfCopy * 35;
+	$tax = $subtotal * 0.07;
+	$shippingHandling = $numberOfCopy * 1.25;
+	$total = $subtotal + $tax + $shippingHandling;
+
+	
+	print ("<p>Operating System: $operatingSys</p>");
+	print ("<p>Number of Copies: $numberOfCopy</p>");
+	print ("<p>Sub-Total:$".number_format($subtotal,2)."</p>");
+	print ("<p>Sales Tax:$".number_format($tax,2)."</p>");
+	print ("<p>Shipping and Handling:$".number_format($shippingHandling,2)."</p>");
+	print ("<p></p>");
+	print ("<p><h2>TOTAL: $".number_format($total,2)."</h2></p>");
+  
+  ?>
+  
+<html>
+<head>
+	<title>SaveTheWorld Software</title>
+	<link rel ="stylesheet" type="text/css" href="sample.css" />
+</head>
+<body>
+
+	<h1>SaveTheWorld: Order Form</h1>
+	<p>To order <strong>SaveTheWorld</strong>, please complete the following order form: </p>
+	<p>
+		<form action = "" method = "POST" >
+			<table>
+			<tr>
+				<td>Choose your operating system:</td>
+				<td>
+					<select name = "os">
+						<option>Linux</option>
+						<option>Macintosh</option>
+						<option>Windows</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Number of copies:</td>
+				<td><input type = "text" size = "5" name = "numCopies" value="0" /></td>
+			</tr>
+			</table>
+			
+			<p>
+			<input type = "submit" value = "Submit Your Order" />
+			</p>
+		</form>
+		
+	</p>
+	
+		
+</body>
+</html>
