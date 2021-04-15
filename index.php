7065e7763cde6bf9ac0ac91142c384a7e0a7ed28
bin1688 <71330321+bin1688@users.noreply.github.com>
2021-04-15 09:15:02 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
-<?php
-	$operatingSys = $_POST['os'];
-	$numberOfCopy = $_POST['numCopies'];
-	$subtotal = $numberOfCopy * 35;
-	$tax = $subtotal * 0.07;
-	$shippingHandling = $numberOfCopy * 1.25;
-	$total = $subtotal + $tax + $shippingHandling;
 
-	
-	print ("<p>Operating System: $operatingSys</p>");
-	print ("<p>Number of Copies: $numberOfCopy</p>");
-	print ("<p>Sub-Total:$".number_format($subtotal,2)."</p>");
-	print ("<p>Sales Tax:$".number_format($tax,2)."</p>");
-	print ("<p>Shipping and Handling:$".number_format($shippingHandling,2)."</p>");
-	print ("<p></p>");
-	print ("<p><h2>TOTAL: $".number_format($total,2)."</h2></p>");
-  
-  ?>
-  
 <html>
 <head>
 	<title>Operating System</title>
@@ -55,3 +37,22 @@
 		
 </body>
 </html>
+<?php
+	$operatingSys = $_POST['os'];
+	$numberOfCopy = $_POST['numCopies'];
+	$subtotal = $numberOfCopy * 35;
+	$tax = $subtotal * 0.07;
+	$shippingHandling = $numberOfCopy * 1.25;
+	$total = $subtotal + $tax + $shippingHandling;
+
+	
+	print ("<p>Operating System: $operatingSys</p>");
+	print ("<p>Number of Copies: $numberOfCopy</p>");
+	print ("<p>Sub-Total:$".number_format($subtotal,2)."</p>");
+	print ("<p>Sales Tax:$".number_format($tax,2)."</p>");
+	print ("<p>Shipping and Handling:$".number_format($shippingHandling,2)."</p>");
+	print ("<p></p>");
+	print ("<p><h2>TOTAL: $".number_format($total,2)."</h2></p>");
+  
+  ?>
+  
